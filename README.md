adb9cc0d11f7f7e0579c969045b53cb26acaa2c4
Ahmed Hani Ibrahim <ahani.fcis@gmail.com>
2016-01-06 17:49:34 +0200

    README updated.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # PySwarmOptimization
 A Python package that implements some of swarm optimization algorithms that are added up together in one package to be used as an API for other projects.
+
+# Algorithms (continuously updating)
+
+## Bat Algorithm
+Source paper: [http://arxiv.org/pdf/1004.4170v1.pdf](http://arxiv.org/pdf/1004.4170v1.pdf) by Xin-She Yang
+
+# Installation
+* git clone https://github.com/AhmedHani/PySwarmOptimization.git
+* cd to the project directory
+* ``` python setup.py install ```
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PySwarmOptimization',
+    version='1.0',
+    packages=['swarm', 'statistics', 'bat_algorithm_package'],
+    url='https://github.com/AhmedHani/PySwarmOptimization',
+    license='GNU GENERAL PUBLIC LICENSE',
+    author='Ahmed Hani Ibrahim',
+    author_email='ahmed.hani.ibrahim@gmail.com',
+    description='A Python package that implements some of swarm optimization algorithms that are added up together in one package to be used as an API for other projects.'
+)
